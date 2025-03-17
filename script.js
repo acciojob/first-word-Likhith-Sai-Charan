0408e042afb685a781ca6c2a224f1afe86ecf533
Likhith Sai Charan Jupudi <143252814+Likhith-Sai-Charan@users.noreply.github.com>
2025-03-17 19:21:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
   // your code here
+	const words=s.split(" ");
+	return words[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+console.log(firstWord('see and stop'))
+console.log(firstWord('Hello World!'))
+
+//const s = prompt("Enter String:");
+//alert(firstWord(s));
